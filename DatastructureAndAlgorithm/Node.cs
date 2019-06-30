5733d9ef19bb4f73bbfbfb91da66a3344ed084ef
Ashish Agarwal <quaintashish@gmail.com>
2019-06-30 14:51:02 +0530

    First commit- Added Node code

---
+++ DatastructureAndAlgorithm/Node.cs
@@ -0,0 +1,8 @@
+﻿namespace DatastructureAndAlgorithm
+{
+    internal class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+}
---
+++ DatastructureAndAlgorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DatastructureAndAlgorithm
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Node firstNode = new Node { Value = 1 };
+            Node secondNode = new Node { Value = 2 };
+
+            Node thirdNode = new Node { Value = 3 };
+
+            // Making node chain
+
+            firstNode.Next = secondNode;
+            secondNode.Next = thirdNode;
+
+            // Printing in chain
+            PrintChain(firstNode);
+        }
+
+        private static void PrintChain(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+
+                node = node.Next;
+            }
+        }
+    }
+}
